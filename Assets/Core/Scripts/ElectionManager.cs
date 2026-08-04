6f727bc28ee886bcc66df86f2d624fcbe24c8bb8
VOXFISH <80233273+VOXFISH@users.noreply.github.com>
2026-08-04 22:37:27 +0900

    feat: 엔딩 영상 구현
    실패 시, 성공 시 큐에 있는 영상 순차 재생

--- Assets/Core/Scripts/ElectionManager.cs
+++ Assets/Core/Scripts/ElectionManager.cs
@@ -27,6 +27,7 @@ public class ElectionManager : MonoBehaviour
     private Cardinal currentWinnerCandidate;
     public Cardinal CurrentWinnerCandidate => currentWinnerCandidate;
     private bool isElected = false;
+    public bool IsElected => isElected;
 
     public void DebugElectPlayer()
     {
